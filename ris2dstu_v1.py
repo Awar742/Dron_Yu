20e46cf19a7bd695ec8a55a0221db02d2ca614bf
Awar742 <71485768+Awar742@users.noreply.github.com>
2025-04-28 22:25:27 +0300

    Update ris2dstu_v1.py

--- ris2dstu_v1.py
+++ ris2dstu_v1.py
@@ -1,39 +1,63 @@
 def authors(au):
-    r=au.split(", ")
-    if len(r)==2:
-        return r[0]+" "+r[1][0]+"."
-    if len(r)==1:
-        return r[0]
+    
+    r = au.strip().split(", ")
+    if len(r) == 2:
+        lastname, firstname = r
+        initials = "".join([name[0] + "." for name in firstname.split()])
+        return f"{lastname} {initials}"
+    return au.strip()
 
 def convert(ris_text):
-    title=""
-    journal=""
-    year=""
-    volume=""
-    doi=""
-    AU=[]
-    for i in ris_text.splitlines():
-        k,v=i.split("  - ")
-        # print(k, v)
-        if k=="T1":
-            title=v
-        if k=="JO":
-            journal=v
-        if k=="PY":
-            year=v
-        if k=="VL":
-            volume=v
-        if k=="SP":
-            sp=v
-        if k=="DO":
-            doi=v
-        if k=="AU":
-            au=v
-            AU.append(authors(au))
-    return "%s %s. // %s. %s. %s. %s. DOI: %s"%(", ".join(AU), title, journal, year, volume, sp, doi)
+    title = ""
+    journal = ""
+    year = ""
+    volume = ""
+    sp = ""
+    doi = ""
+    AU = []
 
-f1=open("S2666165925000286.ris", "r")
-s=f1.read()
-f1.close() # закрити файл
-r=convert(s)
+    for i in ris_text.splitlines():
+        if "  - " not in i:
+            continue  
+        k, v = i.split("  - ", 1)
+        v = v.strip()
+        if k == "T1" or k == "TI":
+            title = v
+        elif k == "JO" or k == "SO":
+            journal = v
+        elif k == "PY":
+            year = v
+        elif k == "VL":
+            volume = v
+        elif k == "SP":
+            sp = v
+        elif k == "DO":
+            doi = v
+        elif k == "AU":
+            AU.append(authors(v))
+
+    
+    parts = []
+    if AU:
+        parts.append(", ".join(AU))
+    if title:
+        parts.append(title)
+    if journal:
+        parts.append("// " + journal)
+    if year:
+        parts.append(year)
+    if volume:
+        parts.append("Т." + volume)
+    if sp:
+        parts.append("С." + sp)
+    if doi:
+        parts.append("DOI: " + doi)
+
+    return ". ".join(parts) + "."
+
+# Основна частина
+with open("S2666165925000286.ris", "r", encoding="utf-8") as f1:
+    s = f1.read()
+
+r = convert(s)
 print(r)
